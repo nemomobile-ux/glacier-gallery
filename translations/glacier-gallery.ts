e027d2f93850f03b9ef86fb8b836b51b0e769ee4
Jozef Mlich <jmlich83@gmail.com>
2022-04-18 10:18:06 +0200

    lupdate

--- translations/glacier-gallery.ts
+++ translations/glacier-gallery.ts
@@ -2,21 +2,16 @@
 <!DOCTYPE TS>
 <TS version="2.1">
 <context>
-    <name>GalleryView</name>
-    <message>
-        <location filename="../src/qml/api/GalleryView.qml" line="84"/>
-        <source>No photo has been taken yet</source>
-        <translation type="unfinished"></translation>
-    </message>
-</context>
-<context>
     <name>ImageEditor</name>
     <message>
+        <location filename="../src/qml/ImageEditor.qml" line="17"/>
         <location filename="../src/qml/ImageEditor.qml" line="17"/>
         <source>Details</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
+        <location filename="../src/qml/ImageEditor.qml" line="26"/>
+        <location filename="../src/qml/ImageEditor.qml" line="41"/>
         <location filename="../src/qml/ImageEditor.qml" line="26"/>
         <location filename="../src/qml/ImageEditor.qml" line="41"/>
         <source>Filename: %1</source>
@@ -25,10 +20,14 @@
     <message>
         <location filename="../src/qml/ImageEditor.qml" line="27"/>
         <location filename="../src/qml/ImageEditor.qml" line="42"/>
+        <location filename="../src/qml/ImageEditor.qml" line="27"/>
+        <location filename="../src/qml/ImageEditor.qml" line="42"/>
         <source>Full path: %1</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
+        <location filename="../src/qml/ImageEditor.qml" line="28"/>
+        <location filename="../src/qml/ImageEditor.qml" line="43"/>
         <location filename="../src/qml/ImageEditor.qml" line="28"/>
         <location filename="../src/qml/ImageEditor.qml" line="43"/>
         <source>Last modified: %1</source>
@@ -36,70 +35,84 @@
     </message>
     <message>
         <location filename="../src/qml/ImageEditor.qml" line="30"/>
+        <location filename="../src/qml/ImageEditor.qml" line="30"/>
         <source>Duration: %1</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
+        <location filename="../src/qml/ImageEditor.qml" line="36"/>
         <location filename="../src/qml/ImageEditor.qml" line="36"/>
         <source>Frame rate: %1 fps</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../src/qml/ImageEditor.qml" line="46"/>
+        <location filename="../src/qml/ImageEditor.qml" line="46"/>
         <source>Orientation: %1˚</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
+        <location filename="../src/qml/ImageEditor.qml" line="47"/>
         <location filename="../src/qml/ImageEditor.qml" line="47"/>
         <source>Exposure time: %1/1000</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../src/qml/ImageEditor.qml" line="48"/>
+        <location filename="../src/qml/ImageEditor.qml" line="48"/>
         <source>fNumber: %1</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
+        <location filename="../src/qml/ImageEditor.qml" line="49"/>
         <location filename="../src/qml/ImageEditor.qml" line="49"/>
         <source>Flash Enabled: %1 </source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../src/qml/ImageEditor.qml" line="50"/>
+        <location filename="../src/qml/ImageEditor.qml" line="50"/>
         <source>Focal Lenght: %1 mm</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
+        <location filename="../src/qml/ImageEditor.qml" line="51"/>
         <location filename="../src/qml/ImageEditor.qml" line="51"/>
         <source>Metering mode: %1</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../src/qml/ImageEditor.qml" line="52"/>
+        <location filename="../src/qml/ImageEditor.qml" line="52"/>
         <source>White balance: %1</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
+        <location filename="../src/qml/ImageEditor.qml" line="53"/>
         <location filename="../src/qml/ImageEditor.qml" line="53"/>
         <source>Camera Manufacturer: %1</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../src/qml/ImageEditor.qml" line="54"/>
+        <location filename="../src/qml/ImageEditor.qml" line="54"/>
         <source>Camera Model: %1</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
+        <location filename="../src/qml/ImageEditor.qml" line="55"/>
         <location filename="../src/qml/ImageEditor.qml" line="55"/>
         <source>Latitude: %1</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../src/qml/ImageEditor.qml" line="56"/>
+        <location filename="../src/qml/ImageEditor.qml" line="56"/>
         <source>Longitude: %1</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
+        <location filename="../src/qml/ImageEditor.qml" line="57"/>
         <location filename="../src/qml/ImageEditor.qml" line="57"/>
         <source>Altitude: %1 m</source>
         <translation type="unfinished"></translation>
@@ -109,10 +122,12 @@
     <name>ImagePage</name>
     <message>
         <location filename="../src/qml/ImagePage.qml" line="48"/>
+        <location filename="../src/qml/ImagePage.qml" line="48"/>
         <source>Show video</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
+        <location filename="../src/qml/ImagePage.qml" line="48"/>
         <location filename="../src/qml/ImagePage.qml" line="48"/>
         <source>Show image</source>
         <translation type="unfinished"></translation>
@@ -122,6 +137,7 @@
     <name>ImageSlideshowPage</name>
     <message>
         <location filename="../src/qml/ImageSlideshowPage.qml" line="47"/>
+        <location filename="../src/qml/ImageSlideshowPage.qml" line="47"/>
         <source>Slideshow</source>
         <translation type="unfinished"></translation>
     </message>
@@ -129,52 +145,68 @@
 <context>
     <name>MainPage</name>
     <message>
-        <location filename="../src/qml/MainPage.qml" line="74"/>
+        <location filename="../src/qml/MainPage.qml" line="65"/>
+        <location filename="../src/qml/MainPage.qml" line="65"/>
+        <source>No photo has been taken yet</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/qml/MainPage.qml" line="81"/>
+        <location filename="../src/qml/MainPage.qml" line="81"/>
         <source>All</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../src/qml/MainPage.qml" line="75"/>
+        <location filename="../src/qml/MainPage.qml" line="82"/>
+        <location filename="../src/qml/MainPage.qml" line="82"/>
         <source>Videos</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../src/qml/MainPage.qml" line="76"/>
+        <location filename="../src/qml/MainPage.qml" line="83"/>
+        <location filename="../src/qml/MainPage.qml" line="83"/>
         <source>Images</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../src/qml/MainPage.qml" line="82"/>
+        <location filename="../src/qml/MainPage.qml" line="89"/>
+        <location filename="../src/qml/MainPage.qml" line="89"/>
         <source>None</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../src/qml/MainPage.qml" line="83"/>
+        <location filename="../src/qml/MainPage.qml" line="90"/>
+        <location filename="../src/qml/MainPage.qml" line="90"/>
         <source>Name</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../src/qml/MainPage.qml" line="84"/>
+        <location filename="../src/qml/MainPage.qml" line="91"/>
+        <location filename="../src/qml/MainPage.qml" line="91"/>
         <source>Modified</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../src/qml/MainPage.qml" line="90"/>
+        <location filename="../src/qml/MainPage.qml" line="97"/>
+        <location filename="../src/qml/MainPage.qml" line="97"/>
         <source>Gallery</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../src/qml/MainPage.qml" line="96"/>
+        <location filename="../src/qml/MainPage.qml" line="103"/>
+        <location filename="../src/qml/MainPage.qml" line="103"/>
         <source>Slideshow</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../src/qml/MainPage.qml" line="109"/>
+        <location filename="../src/qml/MainPage.qml" line="116"/>
+        <location filename="../src/qml/MainPage.qml" line="116"/>
         <source>Filter:</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../src/qml/MainPage.qml" line="152"/>
+        <location filename="../src/qml/MainPage.qml" line="159"/>
+        <location filename="../src/qml/MainPage.qml" line="159"/>
         <source>Sort:</source>
         <translation type="unfinished"></translation>
     </message>
@@ -191,6 +223,7 @@
     <name>SingleImagePage</name>
     <message>
         <location filename="../src/qml/SingleImagePage.qml" line="105"/>
+        <location filename="../src/qml/SingleImagePage.qml" line="105"/>
         <source>Image</source>
         <translation type="unfinished"></translation>
     </message>
@@ -198,6 +231,7 @@
 <context>
     <name>VideoPlayer</name>
     <message>
+        <location filename="../src/qml/VideoPlayer.qml" line="51"/>
         <location filename="../src/qml/VideoPlayer.qml" line="51"/>
         <source>Video</source>
         <translation type="unfinished"></translation>
